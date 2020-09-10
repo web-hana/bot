a1c714cc07f250d5529b1e8ab56f6f1681d17f14
Hana <momenthana@kakao.com>
2020-09-10 20:15:28 +0900

    Create help.js

---
+++ src/commands/help.js
@@ -0,0 +1,6 @@
+const help = (msg) => {
+  msg.channel.send('Help!')
+  return
+}
+
+module.exports = help
--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 
+const { help } = require('./commands')
+
+const commands = {
+  'help': help,
+}
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!')
+client.on('message', async msg => {
+  if (msg.author.bot) return
+  
+  for (const [regexp, command] of Object.entries(commands)) {
+    if (msg.content.match(RegExp(regexp, 'i'))) {
+      await command(msg)
+      return
+    }
   }
 })
 
